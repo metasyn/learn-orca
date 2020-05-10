79187ba27eac14456d45c662ad8aec49caea98a2
Alexander Johnson <alexx.johnson@gmail.com>
2020-05-10 13:28:29 -0700

    Use modified clients.

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 'use strict'
+
+
+window.addEventListener('load', function () {
+
+  console.log('Patching Orca <--> Enfer!')
+  const orcaClient = document.getElementById('orca-iframe').contentWindow.orcaClient
+  const enferClient = document.getElementById('enfer-iframe').contentWindow.enferClient;
+
+  const enferBridge = {
+    send: (msg) => {
+      enferClient.io.onMessage({data: msg})
+    }
+  }
+
+  orcaClient.io.midi.outputDevice = () => enferBridge
+})
